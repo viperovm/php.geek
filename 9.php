0904637986f25690707ef2da2227b6fd2eb951d5
viperovm <s069362949@gmail.com>
2020-02-17 18:01:21 +0100

    lesson complete

--- 9.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo '<p>' . $str . '</p>';
-function getUrlFromStr($str, $arr)
-{
-    return mb_strtolower(str_replace(' ', '_', getTranslite($str, $arr)));
-}
-
-echo '<p>' . getUrlFromStr($str, $arr) . '</p>';
